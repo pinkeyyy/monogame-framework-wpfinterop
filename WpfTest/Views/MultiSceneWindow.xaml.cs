d368965df7b40b9cb6d0110fa718714ddb804d01
MarcStan <marcstan@live.com>
2017-02-23 19:17:53 +0100

    changed one scene to run as low as 20 fps and added description that 30fps and 20fps scenes are not bugged but on purpose

--- WpfTest/Views/MultiSceneWindow.xaml.cs
+++ WpfTest/Views/MultiSceneWindow.xaml.cs
@@ -13,6 +13,7 @@ namespace WpfTest.Views
 			// showcase FPS limit
 			// WPF limits everything to 60 fps max, but we still can run with less WPF if desired
 			CinematicExperience.TargetElapsedTime = TimeSpan.FromSeconds(1 / 30.0);
+			LowFps.TargetElapsedTime = TimeSpan.FromSeconds(1 / 20.0);
 		}
 	}
 }
