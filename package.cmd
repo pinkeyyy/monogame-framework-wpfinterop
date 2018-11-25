211757f77d4107e49bd55849919109c0c3315036
Marc Stan <marcstan@live.com>
2018-11-25 14:50:43 +0100

    make script use version from GlobalAssemblyInfo.cs

--- package.cmd
+++ package.cmd
@@ -1,9 +1,11 @@
 @echo OFF
-if "%1"=="" (
-  set /p version="Enter SemVer: "
-) else (
-version = %1
-)
+
+REM get the version number from file
+for /f "tokens=2 delims=()" %%i in ('findstr /r AssemblyFileVersion.* GlobalVersionInfo.cs') do set version=%%i
+REM format is "x.y.z.0"m drop the first and last 3 chars to get nuget friendly version
+SET version=%version:~1,-3%
+echo Using version %version% for nuget package
+
 cd MonoGame.Framework.WpfInterop
 msbuild MonoGame.Framework.WpfInterop.csproj /p:Configuration=Release
 cd bin\Release
