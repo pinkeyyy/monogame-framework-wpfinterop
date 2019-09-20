6e5138bad1148d9879c2aed037821b7b10e3c056
Marc Stan <marcstan@live.com>
2019-09-20 18:48:21 +0200

    added automated build pipeline

--- GlobalVersionInfo.cs
+++
@@ -1,6 +0,0 @@
-using System.Reflection;
-
-[assembly: AssemblyCompany("MarcStan")]
-[assembly: AssemblyCopyright("Copyright © MarcStan 2016 - 2018")]
-[assembly: AssemblyVersion("1.7")]
-[assembly: AssemblyFileVersion("1.7.1.0")]
