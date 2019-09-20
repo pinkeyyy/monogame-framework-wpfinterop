8f32a4c9e57a6fe5545cc30418dd4b73fe31026e
Marc Stan <marcstan@live.com>
2019-09-20 20:08:12 +0200

    replace version in AssemblyInfo.cs to keep msbuild happy

--- MonoGame.Framework.WpfInterop/Properties/AssemblyInfo.cs
+++ MonoGame.Framework.WpfInterop/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("MonoGame.Framework.WpfInterop")]
@@ -6,6 +6,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyProduct("MonoGame.Framework.WpfInterop")]
 [assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+[assembly: AssemblyCulture("")]  
+[assembly: AssemblyCompany("Marc Stan")]
+[assembly: AssemblyCopyright("Copyright © Marc Stan 2016 - 2019")]
+// version placeholder
 [assembly: ComVisible(false)]
 [assembly: Guid("809f0d79-aedc-4754-a213-fdbe97ccd6d6")]
